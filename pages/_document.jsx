b14384284ae76a70331c77849e644b34d32d4f25
Chihiro Nakashima <mithumiami@gmail.com>
2023-04-06 00:30:11 +0900

    add: GoogleFonts導入

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,7 @@ export default function Document() {
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
       />
+      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500&display=swap" rel="stylesheet" />
 
       </Head>
       <body>
